f28722c6dcda86fcb857774482a878849dde7c0b
urielbp <urielb@gmail.com>
2018-12-27 19:15:15 +0100

    Initial Commit

---
+++ myMovies/FirstViewController.swift
@@ -0,0 +1,20 @@
+//
+//  FirstViewController.swift
+//  myMovies
+//
+//  Created by Uriel Pinheiro on 27/12/2018.
+//  Copyright © 2018 Uriel Pinheiro. All rights reserved.
+//
+
+import UIKit
+
+class FirstViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
---
+++ myMovies/SecondViewController.swift
@@ -0,0 +1,20 @@
+//
+//  SecondViewController.swift
+//  myMovies
+//
+//  Created by Uriel Pinheiro on 27/12/2018.
+//  Copyright © 2018 Uriel Pinheiro. All rights reserved.
+//
+
+import UIKit
+
+class SecondViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
